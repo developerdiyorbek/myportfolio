2a7d35fc06820b3ff273e42a31d25d99f1d1b319
developerdiyorbek <developersulaymonov1907@gmail.com>
2024-07-04 11:09:04 +0500

    deployment

--- components/shared/DownloadBtn.tsx
+++ components/shared/DownloadBtn.tsx
@@ -4,7 +4,7 @@ import Link from "next/link";
 function DownloadBtn() {
   return (
     <Link
-      href="/myresume.pdf"
+      href="/Resume.pdf"
       target="_blank"
       download={true}
       className="block mb-5 animate-pulse"
