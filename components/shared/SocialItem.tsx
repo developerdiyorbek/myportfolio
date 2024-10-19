9eff8b9d7c58cf14543d3020f47df0a58279a34c
developerdiyorbek <developersulaymonov1907@gmail.com>
2024-10-19 19:45:50 +0500

    add socials

--- components/shared/SocialItem.tsx
+++ components/shared/SocialItem.tsx
@@ -13,7 +13,7 @@ function SocialItem({ link, target, Icon, color }: Props) {
     <Link
       href={link}
       target={target}
-      className={`${color} p-[10px] rounded hover:opacity-70 transition-opacity duration-150`}
+      className={`${color} p-[10px] rounded hover:opacity-70 transition-all duration-150 hover:-translate-y-[5px]`}
     >
       <Icon size={20} />
     </Link>
